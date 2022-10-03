35979a1c75efaf7cd80cb3b7f9a7905cc5d5573b
SheykoWk <sheyko.wk@gmail.com>
2022-10-03 13:56:09 -0500

    modify controllers

--- src/todos/todos.controllers.js
+++ src/todos/todos.controllers.js
@@ -2,12 +2,12 @@ const uuid = require('uuid')
 
 const todoDB = [
   {
-    id: 1,
+    id: 'b1ffad08-b0e7-4b6a-8b9e-2f42efc168c0',
     title: "Este es un asdkfjbsdljkfb",
     is_completed: false,
   },
   {
-    id: 2,
+    id: '6af2278d-45f5-49bc-8607-8ff2616d3e0b',
     title: "Este es otro titulo",
     is_completed: false,
   },
